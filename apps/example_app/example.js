2db2ecf91683b581d5154d966d3b451167e6dc2a
Daniel <tequnix@gmail.com>
2013-11-19 18:13:26 +0100

    made console pop up again

--- apps/example_app/example.js
+++ apps/example_app/example.js
@@ -1,4 +1,24 @@
+var gpio = require("pi-gpio"),
+    on = true,
+    intvl;
+    
 console.log("Start!");
-setTimeout(function () {
-    console.log("End!");
-}, 9000);
+
+gpio.open(18, "output", function(err) {
+    
+    intvl = setInterval(function () {
+        gpio.write(18, Number(on));
+        on = !on;
+    }, 200);
+    
+    //process.kill(process.pid, "SIGTERM");
+});
+
+process.on("SIGTERM", function ()  {
+        console.log("End!");
+        clearInterval(intvl);
+        gpio.write(18, 0, function () {
+            gpio.close(18);
+            process.exit();
+        });
+});
